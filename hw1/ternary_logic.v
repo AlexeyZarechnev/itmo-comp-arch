d69cf94571865d8a7eb0b4270ade774d5ee3ca7d
Roman Melnikov <roman.melnikov@serokell.io>
2023-10-05 20:22:28 +0200

    Add HW1

---
+++ hw1/ternary_logic.v
@@ -0,0 +1,27 @@
+module ternary_min(a, b, out);
+  input [1:0] a
+  input [1:0] b;
+  output [1:0] out;
+  // TODO: implementation
+endmodule
+
+module ternary_max(a, b, out);
+  input [1:0] a
+  input [1:0] b;
+  output [1:0] out;
+  // TODO: implementation
+endmodule
+
+module ternary_any(a, b, out);
+  input [1:0] a
+  input [1:0] b;
+  output [1:0] out;
+  // TODO: implementation
+endmodule
+
+module ternary_consensus(a, b, out);
+  input [1:0] a
+  input [1:0] b;
+  output [1:0] out;
+  // TODO: implementation
+endmodule
