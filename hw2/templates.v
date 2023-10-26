e92b7bd69d9f2537a44b8c97511f4ca8718a7cfb
Roman Melnikov <roman.melnikov@serokell.io>
2023-10-26 22:02:36 +0200

    Add HW2

---
+++ hw2/templates.v
@@ -0,0 +1,46 @@
+module alu(a, b, control, res);
+  input [3:0] a, b; // Операнды
+  input [2:0] control; // Управляющие сигналы для выбора операции
+
+  output [3:0] res; // Результат
+  // TODO: implementation
+endmodule
+
+module d_latch(clk, d, we, q);
+  input clk; // Сигнал синхронизации
+  input d; // Бит для записи в ячейку
+  input we; // Необходимо ли перезаписать содержимое ячейки
+
+  output reg q; // Сама ячейка
+  // Изначально в ячейке хранится 0
+  initial begin
+    q <= 0;
+  end
+  // Значение изменяется на переданное на спаде сигнала синхронизации
+  always @ (negedge clk) begin
+    // Запись происходит при we = 1
+    if (we) begin
+      q <= d;
+    end
+  end
+endmodule
+
+module register_file(clk, rd_addr, we_addr, we_data, rd_data, we);
+  input clk; // Сигнал синхронизации
+  input [1:0] rd_addr, we_addr; // Номера регистров для чтения и записи
+  input [3:0] we_data; // Данные для записи в регистровый файл
+  input we; // Необходимо ли перезаписать содержимое регистра
+
+  output [3:0] rd_data; // Данные, полученные в результате чтения из регистрового файла
+  // TODO: implementation
+endmodule
+
+module counter(clk, addr, control, immediate, data);
+  input clk; // Сигнал синхронизации
+  input [1:0] addr; // Номер значения счетчика которое читается или изменяется
+  input [3:0] immediate; // Целочисленная константа, на которую увеличивается/уменьшается значение счетчика
+  input control; // 0 - операция инкремента, 1 - операция декремента
+
+  output [3:0] data; // Данные из значения под номером addr, подающиеся на выход
+  // TODO: implementation
+endmodule
